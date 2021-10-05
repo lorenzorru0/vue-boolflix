c453446c30683a2bcc2ba2de26f78d203f29e1c5
lorenzorru0 <lorenzoorru999@gmail.com>
2021-10-05 15:44:19 +0200

    Add changes

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: Header,
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    background-color: rgb(20, 20, 20);
+}
+</style>
