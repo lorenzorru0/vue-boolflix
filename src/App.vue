f0b222ba44ed0b67dc7cb12c9eacfa03e028c0d4
lorenzorru0 <lorenzoorru999@gmail.com>
2021-10-05 22:15:33 +0200

    Add popular movies and tvs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <div id="app">
+  <div id="app" >
     <Header @stringSearch="getStringSearch" />
     <CardContainer :filmsArray="filmsArray" :tvsArray="tvsArray" />
+    <div class="container">
+      <div class="row">
+        <h2 v-if="popularMovies.length != 0">Popular movies:</h2>
+        <CardFilm class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="movie in popularMovies" :key="movie.id" :film="movie"/>
+      </div>
+      <div class="row">
+        <h2 v-if="popularTvs.length != 0">Popular Tv Series:</h2>
+        <CardTvs class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="tvs in popularTvs" :key="tvs.id" :tvs="tvs"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,18 +19,24 @@
 import axios from 'axios';
 import Header from './components/Header.vue';
 import CardContainer from './components/CardContainer.vue';
+import CardFilm from './components/CardFilm.vue';
+import CardTvs from './components/CardTvs.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
-    CardContainer
+    CardContainer,
+    CardFilm,
+    CardTvs
   },
   data() {
     return {
       stringSearch: '',
       filmsArray: [],
-      tvsArray: []
+      tvsArray: [],
+      popularMovies: [],
+      popularTvs: []
     }
   },
   methods: {
@@ -48,10 +64,48 @@ export default {
         this.tvsArray = resp.data.results;
       });
     }
+  },
+  created() {
+    axios.get('https://api.themoviedb.org/3/movie/popular?page=1', {
+        params: {
+          api_key: 'ed7970cf990eb8d2f2cdf5a51640ead4',
+          language: 'it-IT'
+        }
+      })
+      .then( (resp) => {
+        for (let i = 0; i < 8; i++) {
+          this.popularMovies.push(resp.data.results[i]);
+        }
+      });
+    axios.get('https://api.themoviedb.org/3/tv/popular?page=1', {
+        params: {
+          api_key: 'ed7970cf990eb8d2f2cdf5a51640ead4',
+          language: 'it-IT'
+        }
+      })
+      .then( (resp) => {
+        for (let i = 0; i < 8; i++) {
+          this.popularTvs.push(resp.data.results[i]);
+        }
+      });
   }
 }
 </script>
 
 <style lang="scss">
 @import './assets/style/common.scss';
+
+#app {
+  min-height: calc(100vh - 4.765625rem);
+  background-color: rgb(20, 20, 20);
+  margin-top: 4.765625rem;
+
+  h2 {
+    margin: {
+        top: 1.5625rem;
+        bottom: 1.5625rem;
+    }
+    color: #fff;
+  }
+}
 </style>
--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -3,10 +3,12 @@
         <div class="container">
             <div class="row">
                 <h2 v-if="filmsArray.length != 0">Movies:</h2>
+                <!-- <h2 v-else>There's no match on movies</h2> -->
                 <CardFilm class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="film in filmsArray" :key="film.id" :film="film"/>
             </div>
             <div class="row">
                 <h2 v-if="tvsArray.length != 0">Tv series:</h2>
+                <!-- <h2 v-else>There's no match on tv series</h2> -->
                 <CardTvs class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="tvs in tvsArray" :key="tvs.id" :tvs="tvs"/>
             </div>
         </div>
@@ -33,19 +35,6 @@ export default {
 <style lang="scss" scoped>
 section {
     margin-top: 3.515625rem;
-    min-height: calc(100vh - 3.515625rem);
     background-color: rgb(20, 20, 20);
-
-
-    .container {
-
-        h2 {
-            margin: {
-                top: 3.125rem;
-                bottom: 1.5625rem;
-            }
-            color: #fff;
-        }    
-    }
 }
 </style>
